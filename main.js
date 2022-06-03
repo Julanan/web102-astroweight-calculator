0432d8bd5d90181704060130b22f72b6afe2fea1
Komin <julanan93@gmail.com>
2022-06-03 13:25:45 -0700

    First commit

--- main.js
+++ main.js
@@ -1,1 +1,45 @@
-// Write your JavaScript code here!
+let planets = [
+  ["Pluto", 0.06],
+  ["Neptune", 1.148],
+  ["Uranus", 0.917],
+  ["Saturn", 1.139],
+  ["Jupiter", 2.64],
+  ["Mars", 0.3895],
+  ["Moon", 0.1655],
+  ["Earth", 1],
+  ["Venus", 0.9032],
+  ["Mercury", 0.377],
+  ["Sun", 27.9],
+];
+
+planets.reverse();
+
+let pickPlanet = document.getElementById("planets");
+planets.forEach((planet) => {
+  let option = document.createElement("option");
+  option.value = planet[0];
+  option.innerText = planet[0];
+  pickPlanet.appendChild(option);
+});
+
+function calculateWeight(weight, planetName) {
+  for (let i = 0; i < planets.length; i++) {
+    if (planets[i][0] == planetName) {
+      return parseInt(weight) * planets[i][1];
+    }
+  }
+}
+function handleClickEvent(e) {
+  let userWeight = parseInt(document.getElementById("user-weight").value);
+  let planetName =
+    document.querySelectorAll("#planets option")[
+      document.getElementById("planets").selectedIndex
+    ].textContent;
+  let result = calculateWeight(userWeight, planetName);
+  result = Math.round(result * 100) / 100;
+  document.getElementById("output").innerHTML =
+    "If you were on " + planetName + ", you would weigh " + result + "lbs!";
+}
+document
+  .getElementById("calculate-button")
+  .addEventListener("click", handleClickEvent);
